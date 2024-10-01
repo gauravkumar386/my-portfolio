c183aae08ec7025090d5a8d1c929be2a4cd422df
Gaurav Kumar <gauravk410@gmail.com>
2024-10-02 01:30:19 +0530

    about details changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,12 +19,12 @@ const About = () => {
       <div className={styles.bodyContainer}>
         <div className={styles.leftContainer}>
           <Image src={profile} alt="profile" height={280} width={300} />
-          <p>{`I'm a fullstack developer with 5.3 years of experience 
+          <p>{`I'm a frontend web/mobile developer with 5.3 years of experience 
                     and passion for Javascript, React & all things web development. 
                     The unique combination of creativity, problem solving,
                     UI designing and learning new things drives my excitement and 
                     passion for web development. When I'm not at my PC
-                    I like to spend my time reading, playing guitar and watching football.`}</p>
+                    I like to spend my time reading, playing guitar and watching european league football.`}</p>
         </div>
         <div className={styles.rightContainer}>
           <ul>
